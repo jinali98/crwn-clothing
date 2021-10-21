84a84b662a579e74f3429238015f49e7eb2bfb30
jinali98 <jinalipabasara@gmail.com>
2021-10-21 09:14:25 +0530

    added reselect lib and created selectCartItems function to get cart state from the state

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from "reselect";
+
+//select the cart state from the whole state object
+const selectCart = (state) => state.cart;
+
+//select the cartItems state from the cart state
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce((acc, cartItem) => acc + cartItem.quantity, 0)
+);
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  (cart) => cart.hidden
+);
